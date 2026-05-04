32b3d5cb0a9cc8b59de9273e1cf2f371c9d1c7b7
andreitudor23 <andry.tudor@gmail.com>
2026-05-04 13:36:33 +0300

    Update readme

---
+++ Fortify/app/src/main/java/com/echipappa/fortify/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.echipappa.fortify
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.echipappa.fortify.ui.theme.FortifyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FortifyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FortifyTheme {
+        Greeting("Android")
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# fortify-budget-tracking
-Budget tracking app 
+# \# Fortify 💰
+
+# 
+
+# O aplicație Android pentru monitorizarea bugetului, 
+
+# abonamentelor și prezenței digitale.
+
+# 
+
+# \## Echipa
+
+# \- Andrei Tudor
+
+# 
+
+# \## Tech Stack
+
+# \- Kotlin + Jetpack Compose
+
+# \- Android Studio
+
+# \- Room Database
+
+# 
+
+# \## Status
+
+# 🚧 În dezvoltare
+
