2d29464d66d0039a9371def8451ec830030e3fae
andreitudor23 <andry.tudor@gmail.com>
2026-05-04 13:44:39 +0300

    Add theme

--- Fortify/app/src/main/java/com/echipappa/fortify/ui/theme/Color.kt
+++ Fortify/app/src/main/java/com/echipappa/fortify/ui/theme/Color.kt
@@ -2,10 +2,23 @@ package com.echipappa.fortify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Background colors
+val NavyDark = Color(0xFF0A0E1A)
+val NavyMedium = Color(0xFF0D1117)
+val NavyCard = Color(0xFF131929)
+val NavyCardLight = Color(0xFF1A2235)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Accent colors
+val ElectricBlue = Color(0xFF4A9EFF)
+val ElectricBlueDark = Color(0xFF2979FF)
+val ElectricBlueLight = Color(0xFF7BB8FF)
+
+// Status colors
+val SuccessGreen = Color(0xFF4CAF50)
+val WarningYellow = Color(0xFFFFB300)
+val DangerRed = Color(0xFFFF5252)
+
+// Text colors
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFF8892A4)
+val TextMuted = Color(0xFF4A5568)
